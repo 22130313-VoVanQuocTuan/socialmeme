fe6b41a03f474b14f8e1b1aa1d8ef834fea42a39
Nguyen Anh Tuan <22130311@st.hcmuaf.edu.vn>
2026-06-30 22:37:13 +0700

    Fix missing config.py

--- be/app/main.py
+++ be/app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="SocialMeme API", version="1.0.0")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://smartmeme.io.vn", "https://www.smartmeme.io.vn"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
